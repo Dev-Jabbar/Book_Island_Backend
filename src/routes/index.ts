d8fdab336c428ab974f6fb1658763206296028d6
Momoh Abdul Jabbar <momohabduljabbar@gmail.com>
2024-01-15 15:25:31 +0100

    fouth commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,14 @@
-import express from 'express';
-import bookRouter from './BookRoute';
+import express from "express";
+import bookRouter from "./BookRoute";
 
-import customerRouter from './CustomerRoute';
-import cartrouter from './cartRoute';
-import orderRouter from './orderRoute';
-
+import customerRouter from "./CustomerRoute";
+import cartrouter from "./cartRoute";
+import orderRouter from "./orderRoute";
 
 const router = express.Router();
 
-router.use(customerRouter)
+router.use(customerRouter);
 router.use(bookRouter);
-router.use(cartrouter)
-router.use(orderRouter)
+router.use(cartrouter);
+router.use(orderRouter);
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-
-import routes from './routes';
+import routes from "./routes";
 
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-
 app.use(cors());
 app.use(routes);
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
